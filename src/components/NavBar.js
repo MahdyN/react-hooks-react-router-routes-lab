cc4d3e89ef846320db3c3f06863cea53da19bf62
Mahdy Noureddine <mahdy.a.noureddine@gmail.com>
2022-01-09 17:13:48 -0500

    commit tree 5f79ae6697d3cb8e6f513bf6d7a194d0888468fa

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,7 +2,47 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 
 function NavBar() {
-  return <div>{/*{code here}*/}</div>;
+  return (
+  <div>
+    <ul>
+      <li>
+     <NavLink
+        to="/"
+        exact
+      >
+        Home <br />
+      </NavLink>
+      </li>
+
+      <li>
+      <NavLink
+        to="/movies"
+        exact
+      >
+        Movies <br />
+      </NavLink>
+      </li>
+
+      <li>
+      <NavLink
+        to="/directors"
+        exact
+      >
+        Directors <br />
+      </NavLink>
+      </li>
+
+      <li>
+      <NavLink
+        to="/actors"
+        exact
+      >
+        Actors
+      </NavLink>
+      </li>
+      </ul>
+  </div>
+  );
 }
 
 export default NavBar;
